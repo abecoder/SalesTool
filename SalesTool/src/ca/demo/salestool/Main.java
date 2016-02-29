a31169ee3ef15c29a6a8c67da31c99d3e7702f7b
ABE <ABE@146.245.229.191>
2016-02-29 14:12:28 -0500

    initial checking of salestool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,19 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data = new SalesData();
+		
+		displayGreetings();
+		data.display();
+	}
+	
+	public static void displayGreetings()
+	{
+		System.out.println("Hello, happy sales people!");
+		System.out.println("This app shows sales data!");
+	}
+
+}
